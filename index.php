424e0373c57914d5c04b77ba3940797272d1f7c6
tim <tim.guerre@sfr.fr>
2018-04-20 01:32:31 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Epace Games </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=East+Sea+Dokdo" rel="stylesheet">
+</head>
+<body>
+	
+	<div class="canvas">
+		
+			<img class="titre" src="img/titre_ecrantitre.png">
+			
+			<a class="btn-jouer" href="">Jouer</a>
+			<a class="btn-fuir" href="">Fuir</a>
+			<p class="police_titre" >Maxime Dagniere | Benjamin Guiraud | Tim Guerre | Daniel Leger | Thomas Colombet</p>
+	</div>
+
+
+</body>
+</html>
